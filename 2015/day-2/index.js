2a9115faa97628da2918d538b6afa105441b3ac8
saneag <garstea2002@mail.ru>
2026-03-14 14:52:29 +0200

    feat: add day 2 of 2015 year

---
+++ 2015/day-2/index.js
@@ -0,0 +1,50 @@
+import fs from 'fs';
+
+function partOne() {
+  const input = fs.readFileSync('./partOne.txt', 'utf8');
+
+  let squareFeetOfWrappingPaper = 0;
+
+  input.split('\n').forEach((dimensions) => {
+    const [length, width, height] = dimensions.split('x');
+
+    const firstSide = length * width;
+    const secondSide = length * height;
+    const thirdSide = width * height;
+
+    const smallestSide = Math.min(firstSide, secondSide, thirdSide);
+
+    const wrappingPaperSize = 2 * firstSide + 2 * secondSide + 2 * thirdSide;
+    const additionalWrappingPaper = smallestSide;
+
+    squareFeetOfWrappingPaper += wrappingPaperSize + additionalWrappingPaper;
+  });
+
+  return squareFeetOfWrappingPaper;
+}
+
+console.log(partOne());
+
+function partTwo() {
+  const input = fs.readFileSync('./partTwo.txt', 'utf8');
+
+  let totalLengthOfRibbon = 0;
+
+  input.split('\n').forEach((dimensions) => {
+    const sizes = dimensions.split('x');
+    const [length, width, height] = sizes;
+
+    const sortedSizes = sizes.sort((a, b) => a - b);
+    const minSide = sortedSizes[0];
+    const middleSide = sortedSizes[1];
+
+    const lengthOfRibbonToWrap = minSide * 2 + middleSide * 2;
+    const lengthOfRibbonForTheBow = length * width * height;
+
+    totalLengthOfRibbon += lengthOfRibbonToWrap + lengthOfRibbonForTheBow;
+  });
+
+  return totalLengthOfRibbon;
+}
+
+console.log(partTwo());
