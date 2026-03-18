83b03756bbaf9cf13bbe0eb9f61348436fbdeb7e
saneag <garstea2002@mail.ru>
2026-03-18 17:03:50 +0200

    wip: day 7

---
+++ 2015/day-7/index.js
@@ -0,0 +1,11 @@
+import fs from 'fs';
+
+const input = fs.readFileSync('./input.txt', 'utf8');
+
+function partOne() {}
+
+console.log(partOne());
+
+function partTwo() {}
+
+console.log(partTwo());
